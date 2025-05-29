5f11470e3b60f7a976a1f20b8bf485399d958acc
EightonLeePaulse12 <eightonleepaulse@gmail.com>
2025-05-29 18:42:41 +0200

    start of project done - setup coming next

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
 import { Image } from 'expo-image';
 import { Platform, StyleSheet } from 'react-native';
-
+import { Text } from 'react-native';
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
@@ -18,6 +18,7 @@ export default function HomeScreen() {
       }>
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Welcome!</ThemedText>
+        
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
